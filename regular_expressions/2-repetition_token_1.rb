5a10c55a428718f92559dd71d40fb5986167eb9e
clivetmushipe088 <c.mushipe@alustudent.com>
2024-09-21 05:30:20 -0700

    added

--- regular_expressions/2-repetition_token_1.rb
+++ regular_expressions/2-repetition_token_1.rb
@@ -1,3 +1,20 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hb+t{2,}n/).join
+
+# Define the test patterns
+test_patterns = ["htn", "hbtn", "hbttn", "hbtttn"]
+
+# Get the input argument
+input_arg = ARGV[0]
+
+# Check if the input matches any of the test patterns
+match_found = false
+test_patterns.each do |pattern|
+  if input_arg.match?(pattern)
+    puts "Match found for pattern '#{pattern}'"
+    match_found = true
+  end
+end
+
+# If no match is found, display a message
+puts "No match found" unless match_found
 
