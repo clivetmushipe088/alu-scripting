20e8b964beca18b164701715cf8ecdea3d83ee37
clivetmushipe088 <c.mushipe@alustudent.com>
2024-09-21 05:08:33 -0700

    added

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,14 +1,18 @@
 #!/usr/bin/env ruby
 
+# Check if an argument is passed
 if ARGV.length != 1
   puts "Usage: #{$0} <string>"
   exit 1
 end
 
+# Assign the argument to a variable
 input_string = ARGV[0]
 
+# Regular expression to match the desired pattern
 pattern = /^hbt+n$/
 
+# Check if the input matches the pattern
 if input_string.match(pattern)
   puts input_string
 else
